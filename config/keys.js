0ce54e4b763dff588e7b19fa4960b07b9130448b
ahmedaof <49912597+ahmedaof@users.noreply.github.com>
2019-04-23 16:12:22 +0200

    Add files via upload

---
+++ config/keys.js
@@ -0,0 +1,8 @@
+// add this file to .gitignore
+
+module.exports = {
+        google: {
+            clientID: '1067071802055-6outk3kaudr8ncshj0bkmu3ho9u7kivd.apps.googleusercontent.com',
+            clientSecret: 'HrqsaFqxLQzlIQqNNRTIgU6B'
+        }
+};
